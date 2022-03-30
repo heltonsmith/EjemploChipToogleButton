a98f9d227d889e43ab37f235ef6c0120a9ac661f
Helton Bustos <heltonsmith@hotmail.com>
2022-03-29 21:01:22 -0300

    Initial commit

---
+++ app/src/main/java/com/heltonbustos/ejemplochiptooglebutton/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.heltonbustos.ejemplochiptooglebutton
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.chip.Chip
+import com.heltonbustos.ejemplochiptooglebutton.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var bi: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        bi = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bi.root)
+
+        bi.chipGrupo.setOnCheckedChangeListener { group, checkedId ->
+            val chip = bi.chipGrupo.findViewById<Chip>(checkedId)
+            bi.chipGrupo.removeView(chip)
+        }
+
+        bi.toggleButton.setOnClickListener {
+            Toast.makeText(this, "Selección: ${bi.toggleButton.text}", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
